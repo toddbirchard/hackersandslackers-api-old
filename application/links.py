896dfccebb29b8e4a7ccbef4deebf06d90883906
Todd Birchard <toddbirchard@gmail.com>
2019-02-05 20:16:52 -0500

    refactored link embed module, sanitized link data

--- application/links.py
+++ application/links.py
@@ -7,10 +7,26 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import pprint
+from datetime import datetime as dt
 
 
-def get_alyien_extract(url):
-    """Extract using Alyien API."""
+def sanitize_data(obj):
+    """Sanitizes link embed data to be human-readable."""
+    # Clean URL to be easily readable.
+    displayurl = obj['url'].split('://')[0]
+    displayurl = displayurl.replace('www.', '')
+    obj['displayurl'] = displayurl
+    # Reformat date
+    published = dt.strftime(obj['publihDate'], '%m/%d/%Y %p')
+    obj['publishDate'] = published
+    # Format tags
+    tags = [lambda x: '#' + x.replace(' ', ''), obj['tags']]
+    obj['tags'] = tags
+    return obj
+
+
+def get_aylien_extract(url):
+    """Extract url information using aylien API."""
     headers = {
         'X-AYLIEN-TextAPI-Application-Key': r.get('aylien_app_key'),
         'X-AYLIEN-TextAPI-Application-ID': r.get('aylien_app_id'),
@@ -25,8 +41,8 @@ def get_alyien_extract(url):
     return req.json()
 
 
-def get_alyien_summary(url):
-    """Extract using Alyien API."""
+def get_aylien_summary(url):
+    """Generate a 3-sentence summary of the URL using Aylien."""
     headers = {
         'X-AYLIEN-TextAPI-Application-Key': r.get('aylien_app_key'),
         'X-AYLIEN-TextAPI-Application-ID': r.get('aylien_app_id')
@@ -42,24 +58,34 @@ def get_alyien_summary(url):
     return summary
 
 
-def make_preview(preview_obj):
-    """Create post preview html from link preview JSON."""
-    if preview_obj is not None:
-        preview_html = '<div class="link-preview"> \
-                            <a href="' + str(preview_obj['url']) + '"> \
-                                <div class="link-info"> \
-                                    <div class="link-preview-image"> \
-                                        <img src="' + str(preview_obj['image']) + '"></div> \
-                                        <div class="detail-stack"> \
-                                            <h4 class="title-desktop">' + str(preview_obj['title']) + '</h4> \
-                                            <p>' + str(preview_obj['summary']) + '</p> \
-                                            <h4 class="title-mobile">' + str(preview_obj['url']) + '</h4> \
-                                            <span class="url-info"><i class="far fa-link"></i>' + str(preview_obj['url']) + '</span> \
-                                        </div> \
-                                    </div> \
-                                </a> \
-                            </div>'
-    return preview_html
+def make_preview(linkpreview):
+    """Create post preview HTML from link preview JSON."""
+    if linkpreview is not None:
+        return render_template('linkpreview.html', data=linkpreview)
+    return None
+
+
+def generate_url_metadata(url, posturl):
+    """Create link preview metadata object.
+
+    1. Create extract using Aylien Extract API.
+    2. Generate URL summary using Aylien Summary API.
+    3. Add simple metadata to object.
+    4. Sanitize data.
+    5. Create HTML embed using Jinja Template.
+    6. Return embed.
+    """
+    preview_obj = get_aylien_extract(url)
+    summary = get_aylien_summary(url)
+    preview_obj['summary'] = summary
+    preview_obj['url'] = url
+    preview_obj['lynxorigin'] = posturl
+    preview_obj.pop('article', None)
+    preview_obj = sanitize_data(preview_obj)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(preview_obj)
+    preview_embed = make_preview(preview_obj)
+    return preview_embed
 
 
 def get_links(url):
@@ -71,48 +97,32 @@ def get_links(url):
         'Access-Control-Max-Age': '3600',
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
-    endpoint = r.get('endpoint')
+    # endpoint = r.get('endpoint')
     req = requests.get(url, headers=headers)
     soup = BeautifulSoup(req.text, 'html.parser')
-    tags = soup.select('.post-content a')
+    htmltags = soup.select('.post-content a')
     link_arr = []
-    for tag in tags:
+    for tag in htmltags:
         link = tag.get('href')
-        preview_obj = get_alyien_extract(link)
-        summary = get_alyien_summary(link)
-        preview_obj['summary'] = summary
-        preview_obj['url'] = link
-        preview_obj['lynxorigin'] = url
-        preview_obj.pop('article', None)
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(preview_obj)
-        preview_embed = make_preview(preview_obj)
-        link_arr.append(preview_embed)
+        preview = generate_url_metadata(link, url)
+        link_arr.append(preview)
     print('link_arr = ', link_arr)
     return link_arr
 
 
-@app.route('/lynx', methods=['GET', 'POST'])
-def entry():
-    """App Entry Point."""
-    print('HERP', r.get('aylien_app_id'))
-    print('DERP', r.get('aylien_app_key'))
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        }
+@app.route('/linkembed', methods=['GET', 'POST'])
+def linkembed():
+    """Link embed entry point."""
     uri = r.get('uri')
-    '''query = r.get('query')
-    query_like = r.get('query_like')'''
     domain = r.get('domain')
     post_database = database.LynxData()
     posts = post_database.fetch_records()
     for post in posts:
-        url = domain + post['slug']
-        link_embeds = get_links(url)
-        page_html = ''.join(str(x) for x in link_embeds)
-        print('page_html = ', page_html)
-        post_database.update_post(uri, post['slug'], page_html)
-        preview_html = make_preview(post)
-        print('postpreview = ', preview_html)
+        posturl = domain + post['slug']
+        link_embeds = get_links(post)
+        preview = generate_url_metadata(post, posturl)
+        # page_html = ''.join(str(x) for x in link_embeds)
+        # print('page_html = ', page_html)
+        # post_database.update_post(uri, post['slug'], page_html)
+        print('preview = ', preview)
     return render_template('layout.html')
